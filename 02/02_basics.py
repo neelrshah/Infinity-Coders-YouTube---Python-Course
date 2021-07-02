71e89d8a0a7ec08d20b749eca9e0194fad0a6f49
Neel Shah <64684048+neelrshah@users.noreply.github.com>
2021-07-02 10:37:33 +0530

    02 Video

---
+++ 02/02_basics.py
@@ -0,0 +1,23 @@
+# python basics (variables, Comments)
+# python is easy to learn
+
+'''
+python 
+is 
+easy 
+to learn
+'''
+
+name = "Python"
+
+num = 10
+# print function
+# print("hello")
+# print(name)
+
+
+# user input
+
+num2 = int(input("Enter num 2:"))
+print(num2)
+print(type(num2))
