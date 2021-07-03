ce52e5fc88a0e6807db8f958611faf24f2c04f40
Neel Shah <64684048+neelrshah@users.noreply.github.com>
2021-07-03 13:19:37 +0530

    03_Datatypes added

---
+++ 03/03_Datatypes.py
@@ -0,0 +1,50 @@
+
+# Numeric datatypes
+
+    # integer
+# num = 10
+# print(type(num))
+
+#     # float 
+# num2 = 10.25
+# print(type(num2))
+
+#     # complex
+# num3 = 1 + 2j
+# print(type(num3))
+
+
+# Dictionary
+# d = {1 : "Python" , 2 : "Java"}
+# print(d)
+# print(type(d))
+
+
+# Boolean
+# a = True
+# print(type(a))
+
+
+#string
+# ch = "Welcome"
+# print(type(ch))
+
+
+#set
+# s = {1,2,3,4,5}
+# print(type(s))
+
+# t = {1,2,1,4}
+# print(t)
+
+
+#list
+# li = [1,2,3,"A","B"]
+# print(li)
+# print(type(li))
+
+
+#tuple
+tup = (1,2,3)
+print(tup)
+print(type(tup))
