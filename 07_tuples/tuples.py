c8b2b74460d99289cb148e82cadccd5e42e0ac4c
Neel Shah <64684048+neelrshah@users.noreply.github.com>
2021-07-06 12:37:14 +0530

    tuples added

---
+++ 07_tuples/tuples.py
@@ -0,0 +1,50 @@
+# Basics of tuple
+
+
+# creating a tuple
+# t = (1,2,3,4)
+
+
+# # printing a tuple
+# print(t)
+
+
+
+# t = (1,2,3,"A",10.25)
+# print(t)
+
+# t2 = (1,1,2,1,3,4)
+# print(t2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# accessing elements of tuple and slicing
+# t2 = (1,2,3,4,5)
+# print(t2[0])
+# print(t2[0:3])
+# print(t2[-1])
+
+
+
+# basic functions of tuple
+# len(), count(), index()
+
+# t = (1,3,6,2,4)
+# print(len(t))
+# print(t.count(3))
+# print(t.index(6))
