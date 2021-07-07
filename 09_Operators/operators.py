63fbf09ac2ef51148aaab97848d1c6c84e9da29b
Neel Shah <64684048+neelrshah@users.noreply.github.com>
2021-07-07 14:57:17 +0530

    operators added

---
+++ 09_Operators/operators.py
@@ -0,0 +1,90 @@
+# Operators in Python
+
+# ARITHEMATIC OPERATORS
+# +, -, *, /, **, // , %
+
+# a = 5
+# b = 10
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+
+# print(a ** b)
+# print(a % b)
+
+
+
+
+
+
+# Assignment Operators
+#  =, +=, -=, *=, /=
+
+# a = 5
+# b = 10
+
+# a += b  # a = a+b
+# print(a)
+
+
+
+
+
+
+# Comparison Operators
+# 1. Equal( == )
+# 2. Not Equal( != )
+# 3. Greater than( > )
+# 4. Less than ( < )
+# 5. Greater than equal to ( >= )
+# 6. Less than equal to( <= )
+
+
+# a = 5
+# b = 10
+
+# print(a >= b)
+
+
+
+
+
+
+
+
+
+
+# Logical Operators
+# and, or, not
+
+# a = True
+# b = False
+# c = True
+
+
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+
+
+
+
+
+# Identity Operators
+# is, is not
+
+
+# a = 5
+# b = 5
+
+# print(a is b)
+
+# Membership Operators
+# in, not in
+
+li= [1,2,3,4]
+print(2 in li)
