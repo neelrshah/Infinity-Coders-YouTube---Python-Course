528183d83301a4c89731d69242ea2641983a33ab
Neel Shah <64684048+neelrshah@users.noreply.github.com>
2021-07-08 21:08:12 +0530

    loops added

---
+++ 11_Loops/loops.py
@@ -0,0 +1,30 @@
+# Loops in Python
+# Range Function
+
+# range(0,10)
+
+
+
+# for loop
+
+# for i in range(0,10):
+#     print(i)
+
+
+
+
+# for loop for list
+
+# li = [1,2,3,"A",10.3]
+# for i in li:
+#     print(i)
+
+
+
+
+# while loop
+
+i = 0
+while i <= 10:
+    print(i)
+    i = i+1
