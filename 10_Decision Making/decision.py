f50731c20103fefd9a0c8d8551fbcc51c5fbfbce
Neel Shah <64684048+neelrshah@users.noreply.github.com>
2021-07-08 09:19:27 +0530

    decision making added

---
+++ 10_Decision Making/decision.py
@@ -0,0 +1,30 @@
+# Decision Making
+
+# if Statement
+# a = 10
+# if a == 10:
+#     print(a)
+
+
+
+# if else Statement
+
+
+# a = 5
+# if a % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+
+# elif 
+
+num = 0
+
+if num == 0:
+    print("number is 0")
+elif num > 0:
+    print("positive number")
+else:
+    print("Negative Number")
