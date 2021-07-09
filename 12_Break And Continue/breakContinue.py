fd5fc1d58d7ef82d1a37ab64bc3176f3e1df39b4
Neel Shah <64684048+neelrshah@users.noreply.github.com>
2021-07-09 15:12:37 +0530

    break and continue added

---
+++ 12_Break And Continue/breakContinue.py
@@ -0,0 +1,29 @@
+# pass statement
+
+# num = 2
+# if num == 2:
+#     pass
+
+
+
+# BREAK STATEMENT
+
+# for i in range(0,10):
+#     if i == 5:
+#         break
+#     print(i)
+
+
+
+
+
+
+
+
+
+# CONTINUE STATEMENT
+
+for i in range(0,10):
+    if i % 2 == 0:
+        continue
+    print(i)
